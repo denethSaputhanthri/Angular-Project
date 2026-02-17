e67ed33265344384e901963c5bd2b969798a1ad8
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-17 15:26:21 +0530

    feat: add customer, item, and user management components
    - Implemented Addcustomer component with associated HTML, CSS, and spec files.
    - Implemented Viewcustomer component with associated HTML, CSS, and spec files.
    - Implemented Additem component with associated HTML, CSS, and spec files.
    - Implemented Item component with associated HTML, CSS, and spec files.
    - Implemented Viewitem component with associated HTML, CSS, and spec files.
    - Implemented Login component with associated HTML, CSS, and spec files.
    - Implemented Register component with associated HTML, CSS, and spec files.
    - Added Home and Dashboard components to the app.
    - Updated app module to include new components.

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
+import { Home } from "./page/home/home";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
