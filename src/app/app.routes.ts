ba8f6c309e0036e7f39b7b025b7b628396bb6a7c
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-17 15:56:44 +0530

    feat: implement routing and add customer, item, and order components with styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,72 @@
 import { Routes } from '@angular/router';
+import { Login } from './page/login/login';
+import { Dashboard } from './page/dashboard/dashboard';
+import { Item } from './page/item/item';
+import { Customer } from './page/customer/customer';
+import { Order } from './page/order/order';
+import { Addcustomer } from './page/customer/addcustomer/addcustomer';
+import { Viewcustomer } from './page/customer/viewcustomer/viewcustomer';
+import { Additem } from './page/item/additem/additem';
+import { ViewOrder } from './page/order/view-order/view-order';
+import { AddOrder } from './page/order/add-order/add-order';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"",
+        component:Login,
+    },
+    {
+        path: "/login",
+        component:Login,
+    },
+    {
+        path:"/dashboard",
+        component:Dashboard,
+        children:[
+        {
+            path:"/customer",
+            component:Customer,
+            children:[
+                {
+                    path:"/addcustomer",
+                    component:Addcustomer,
+                },
+                {
+                    path:"/viewcustomer",
+                    component:Viewcustomer,
+                },
+            ]
+        },
+        {
+            path:"/item",
+            component:Item,
+            children:[
+                {
+                    path:"/additem",
+                    component:Additem,
+                },
+                {
+                    path:"/viewitem",
+                    component:ViewOrder,
+                },
+            ]
+        },
+        {
+            path:"/order",
+            component:Order,
+            children:[
+                {
+                    path:"/addorder",
+                    component:AddOrder,
+                },
+                {
+                    path:"/vieworder",
+                    component:ViewOrder,
+                },
+            ]
+        },
+        ]
+
+    },
+
+];
